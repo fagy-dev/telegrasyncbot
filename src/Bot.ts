933ce1297c20b23eed3921423040a65696be817c
Davide Wayan Mores <racecondition99@gmail.com>
2024-10-07 11:44:18 +0200

    New function for calling the Telegram Bot API

--- src/Bot.ts
+++ src/Bot.ts
@@ -7,8 +7,13 @@ export class Bot {
         this.url = `https://api.telegram.org/bot${token}`
     }
 
+    private async callTelegramAPI(methodName: string) {
+        const response = await fetch(`${this.url}/${methodName}`)
+        const result = await response.json()
+        return result.result
+    }
+
     public async getMe(): Promise<User> {
-        const response = await fetch(`${this.url}/getMe`) 
-        return JSON.parse(await response.text()).result
+        return this.callTelegramAPI('getMe')
     }
 }
