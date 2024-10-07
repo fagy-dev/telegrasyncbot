f6366fccc8077abe2efe182a9119cd5678f1cb30
Davide Wayan Mores <racecondition99@gmail.com>
2024-10-07 16:40:57 +0200

    Update: any type

--- src/types/Update.ts
+++ src/types/Update.ts
@@ -1,26 +1,28 @@
 export type Update = {
     update_id: number
-    message?/*: Message*/
-    edited_message?/*: Message*/
-    channel_post?/*: Message*/
-    edited_channel_post?/*: Message*/
-    business_connection?/*: BusinessConnection*/
-    business_message?/*: Message*/
-    edited_business_message?/*: Message*/
-    deleted_business_messages?/*: BusinessMessagesDeleted*/
-    message_reaction?/*: MessageReactionUpdated*/
-    message_reaction_count?/*: MessageReactionCountUpdated*/
-    inline_query?/*: InlineQuery*/
-    chosen_inline_result?/*: ChosenInlineResult*/
-    callback_query?/*: CallbackQuery*/
-    shipping_query?/*: ShippingQuery*/
-    pre_checkout_query?/*: PreCheckoutQuery*/
-    purchased_paid_media?/*: PaidMediaPurchased*/
-    poll?/*: Poll*/
-    poll_answer?/*: PollAnswer*/
-    my_chat_member?/*: MyChatMember*/
-    chat_member?/*: ChatMember*/
-    chat_join_request?/*: ChatJoinRequest*/
-    chat_boost?/*: ChatBoostUpdated*/
-    removed_chat_boost?/*: ChatBoostRemoved*/
+    message?: any/*: Message*/
+    edited_message?: any/*: Message*/
+    channel_post?: any/*: Message*/
+    edited_channel_post?: any/*: Message*/
+    business_connection?: any/*: BusinessConnection*/
+    business_message?: any/*: Message*/
+    edited_business_message?: any/*: Message*/
+    deleted_business_messages?: any/*: BusinessMessagesDeleted*/
+    message_reaction?: any/*: MessageReactionUpdated*/
+    message_reaction_count?: any/*: MessageReactionCountUpdated*/
+    inline_query?: any/*: InlineQuery*/
+    chosen_inline_result?: any/*: ChosenInlineResult*/
+    callback_query?: any/*: CallbackQuery*/
+    shipping_query?: any/*: ShippingQuery*/
+    pre_checkout_query?: any/*: PreCheckoutQuery*/
+    purchased_paid_media?: any/*: PaidMediaPurchased*/
+    poll?: any/*: Poll*/
+    poll_answer?: any/*: PollAnswer*/
+    my_chat_member?: any/*: MyChatMember*/
+    chat_member?: any/*: ChatMember*/
+    chat_join_request?: any/*: ChatJoinRequest*/
+    chat_boost?: any/*: ChatBoostUpdated*/
+    removed_chat_boost?: any/*: ChatBoostRemoved*/
 }
+
+export type UpdateType = keyof Omit<Update, "update_id">
