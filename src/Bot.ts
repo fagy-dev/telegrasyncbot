28e2c2919e98321f4fa3f27f384daa6ed9933cea
Davide Wayan Mores <racecondition99@gmail.com>
2024-10-07 16:41:18 +0200

    getUpdates

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,3 +1,4 @@
+import { Update, UpdateType } from "./types/Update.ts";
 import { User } from "./types/User.ts";
 
 export class Bot {
@@ -7,13 +8,36 @@ export class Bot {
         this.url = `https://api.telegram.org/bot${token}`
     }
 
-    private async callTelegramAPI(methodName: string) {
-        const response = await fetch(`${this.url}/${methodName}`)
+    private async callTelegramAPI(methodName: string, params?: Record<string, string>) {
+        let input: string = `${this.url}/${methodName}`
+
+        if (params) {
+            const queryParams = new URLSearchParams(params).toString()
+
+            console.log(queryParams)
+            input += `?${queryParams}`
+        }
+
+        const response = await fetch(input)
         const result = await response.json()
         return result.result
     }
 
+
+    public async getUpdates(params?: { offset?: number, limit?: number, timeout?: number, allowedUpdates?: UpdateType[] }): Promise<Update[]> {
+        const processedParams: Record<string, string> = {}
+
+        if (params) {
+            if (params.offset) processedParams['offset'] = params.offset.toString()
+            if (params.limit) processedParams['limit'] = params.limit.toString()
+            if (params.timeout) processedParams['timeout'] = params.timeout.toString()
+            if (params.allowedUpdates) processedParams['allowed_updates'] = params.allowedUpdates.toString()
+        }
+
+        return await this.callTelegramAPI('getUpdates', processedParams)
+    }
+
     public async getMe(): Promise<User> {
-        return this.callTelegramAPI('getMe')
+        return await this.callTelegramAPI('getMe')
     }
 }
