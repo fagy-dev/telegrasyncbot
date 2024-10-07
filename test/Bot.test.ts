3ef43851391b42c602d67dbc9ea9049e9059ff60
Davide Wayan Mores <racecondition99@gmail.com>
2024-10-07 16:44:03 +0200

    Remove an unused variable

--- test/Bot.test.ts
+++ test/Bot.test.ts
@@ -3,7 +3,6 @@ import { expect } from "@std/expect";
 import { Bot } from "../src/Bot.ts";
 
 await load({export: true})
-const token = Deno.env.get('TOKEN')
 const bot = new Bot(Deno.env.get('TOKEN') ?? '');
 
 Deno.test("getMe", async () => {
