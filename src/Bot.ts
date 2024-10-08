83a6dc999fa5507db2213ff2c7e07719c8064598
Davide Wayan Mores <racecondition99@gmail.com>
2024-10-08 17:43:44 +0200

    Add methods: forwardMessage and forwardMessages

--- src/Bot.ts
+++ src/Bot.ts
@@ -110,4 +110,26 @@ export class Bot {
   }): Promise<Message> {
     return await this.callTelegramAPI('sendMessage', params)
   }
+
+  public async forwardMessage(params: {
+    chat_id: number | string
+    message_thread_id?: number
+    from_chat_id: number | string
+    disable_notification?: boolean
+    protect_content?: boolean
+    message_id: number
+  }): Promise<Message> {
+    return await this.callTelegramAPI('forwardMessage', params)
+  }
+
+  public async forwardMessages(params: {
+    chat_id: number | string
+    message_thread_id?: number
+    from_chat_id: number | string
+    message_ids: number[]
+    disable_notification?: boolean
+    protect_content?: boolean
+  }): Promise<Message> {
+    return await this.callTelegramAPI('forwardMessages', params)
+  }
 }
