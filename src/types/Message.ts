391afdd6c43effcc78049be4ef19b9e74bbc4c1b
Davide Wayan Mores <racecondition99@gmail.com>
2024-10-08 12:06:23 +0200

    Add type: InaccessibleMessage

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,3 +1,11 @@
+import { Chat } from "./Chat.ts";
+
 export type MessageId = {
     message_id: number
+}
+
+export type InaccessibleMessage = {
+    chat: Chat
+    message_id: number
+    date: 0
 }
